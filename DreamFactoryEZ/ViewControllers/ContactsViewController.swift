ac3c884c0ad4917e3d51c6368f8b593b051c2aae
Eric Elfner <eje@geoderanch.com>
2016-05-06 16:48:43 -0400

    Added contact sections and searching.

--- DreamFactoryEZ/ViewControllers/ContactsViewController.swift
+++ DreamFactoryEZ/ViewControllers/ContactsViewController.swift
@@ -12,7 +12,7 @@ protocol GroupDependent {
     func resetCurrentGroupTo(group:NSNumber?)
 }
 
-class ContactsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, ContactsDelegate {
+class ContactsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, ContactsDelegate, UISearchBarDelegate {
     let kGroupsSegue = "GroupsSegue"
     let kSignInSegue = "SignInSegue"
     
@@ -24,8 +24,13 @@ class ContactsViewController: UIViewController, UITableViewDataSource, UITableVi
     private let dataAccess = DataAccess.sharedInstance
 
     private var contacts = [ContactRecord]()
+    private var contactsSectionsAlpha = [String]()
+    private var contactsBySection = [String: [ContactRecord]]()
+    private var contactsMatchingSearch = [ContactRecord]()
+
     private var currentGroup:GroupRecord? = nil
-    
+    private var bIsSearching = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -102,17 +107,69 @@ class ContactsViewController: UIViewController, UITableViewDataSource, UITableVi
     // MARK: ContactsDelegate
     func setContacts(contacts: [ContactRecord]) {
         self.contacts = contacts
-        self.tableView.reloadData()
+        contactsSectionsAlpha = [String]()
+        contactsBySection = [String: [ContactRecord]]()
+
+        for contact in contacts {
+            let name = contact.fullName
+            let firstChar = name.substringToIndex(name.startIndex.advancedBy(1, limit: name.endIndex))
+            if contactsBySection.keys.contains(firstChar) {
+                contactsBySection[firstChar]!.append(contact)
+            }
+            else {
+                var alphaSection = [ContactRecord]()
+                alphaSection.append(contact)
+                contactsSectionsAlpha.append(firstChar)
+                contactsBySection[firstChar] = alphaSection
+            }
+        }
+        tableView.setContentOffset(CGPointZero, animated: true)
+        tableView.reloadData()
     }
     func dataAccessError(error: NSError?) {
         if let error = error {
             print("Error: \(error)")
         }
     }
-    
+    // MARK: - UISearchBarDelegate
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchText.isEmpty {
+            bIsSearching = false
+        }
+        else {
+            bIsSearching = true
+            let searchString = searchText.uppercaseString
+            contactsMatchingSearch.removeAll()
+            
+            for contact in contacts {
+                if contact.fullName.uppercaseString.containsString(searchString) {
+                    contactsMatchingSearch.append(contact)
+                }
+            }
+        }
+        tableView.reloadData()
+    }
+
     // MARK: UITableViewDataSource
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        if bIsSearching {
+            return 1
+        }
+        return contactsSectionsAlpha.count
+    }
+
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contacts.count
+        var rowCount = 0
+        if bIsSearching {
+             rowCount = contactsMatchingSearch.count
+        }
+        else {
+            if let sectionContacts = contactsBySection[contactsSectionsAlpha[section]] {
+                rowCount = sectionContacts.count
+            }
+        }
+        return rowCount
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier(kCellID)
@@ -120,8 +177,97 @@ class ContactsViewController: UIViewController, UITableViewDataSource, UITableVi
         if (cell == nil) {
             cell = UITableViewCell(style: .Default, reuseIdentifier: kCellID)
         }
-        let contact = contacts[indexPath.row]
-        cell!.textLabel?.text = contact.fullName
+        var contactName = "(no-name)"
+        if let contact = contactForIndexPath(indexPath) {
+            contactName = contact.fullName
+        }
+        cell!.textLabel?.text = contactName
         return cell!
     }
+    private func contactForIndexPath(indexPath:NSIndexPath) -> ContactRecord? {
+        var contact:ContactRecord? = nil
+        if bIsSearching {
+            contact = contactsMatchingSearch[indexPath.row]
+        }
+        else {
+            let sectionAlpha = contactsSectionsAlpha[indexPath.section]
+            if let sectionContacts = contactsBySection[sectionAlpha] {
+                contact = sectionContacts[indexPath.row]
+            }
+        }
+        return contact
+    }
+    // MARK: - UITableViewDelegate
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if bIsSearching {
+            return nil
+        }
+        else {
+            return contactsSectionsAlpha[section]
+        }
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        view.tintColor = UIColor(red: 210/255.0, green: 225/255.0, blue: 239/255.0, alpha: 1.0)
+    }
+    
+//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//        let record = recordForIndexPath(indexPath)
+//        self.navBar.disableAllTouch()
+//        
+//        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+//        showContactViewControllerForRecord(record)
+//    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+//        if editingStyle == .Delete {
+//            if isSearch {
+//                let record = displayContentArray[indexPath.row]
+//                let index = record.lastName.substringToIndex(record.lastName.startIndex.advancedBy(1)).uppercaseString
+//                var displayArray = contactSectionsDictionary[index]!
+//                displayArray.removeObject(record)
+//                if displayArray.count == 0 {
+//                    // remove tile header if there are no more tiles in that group
+//                    alphabetArray.removeObject(index)
+//                }
+//                contactSectionsDictionary[index] = displayArray
+//                
+//                // need to delete everything with references to contact before
+//                // removing contact its self
+//                removeContactWithContactId(record.id)
+//                
+//                displayContentArray.removeAtIndex(indexPath.row)
+//                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+//            } else {
+//                let sectionLetter = alphabetArray[indexPath.section]
+//                var sectionContacts = contactSectionsDictionary[sectionLetter]!
+//                let record = sectionContacts[indexPath.row]
+//                
+//                sectionContacts.removeAtIndex(indexPath.row)
+//                contactSectionsDictionary[sectionLetter] = sectionContacts
+//                
+//                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+//                if sectionContacts.count == 0 {
+//                    alphabetArray.removeAtIndex(indexPath.section)
+//                }
+//                
+//                removeContactWithContactId(record.id)
+//            }
+//        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
